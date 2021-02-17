35d94b2bfeceae22d2eb89fa7562b65a4593f6be
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-02-17 12:04:57 +0530

    Completed stage 2 :D

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime
 
-GENDER = "_YOUR_GENDER_"
+GENDER = "YOUR_GENDER"
 WEIGHT_IN_KG = YOUR_WEIGHT
-HEIGHT_IN_CM = YOUR_HEIGHT
+HEIGHT_IN_CM =  YOUR_HEIGHT
 AGE = YOUR_AGE
 
 APP_ID = "YOUR_APPID"
@@ -25,3 +25,21 @@ parameters = {
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
+#-------------Step2------
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+sheety_endpoint = "https://api.sheety.co/331582b0270ee0406a0838223a682e5f/myWorkouts/workouts"
+print(result['exercises'])
+for i in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": i["name"].title(),
+            "duration": i["duration_min"],
+            "calories": i["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheety_endpoint, json=sheet_inputs)
+    print(sheet_response.text)
